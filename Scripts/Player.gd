a3aae57e43933f654066c720e815281c8f79b615
Jenny <untmdsprt@gmail.com>
2018-07-08 14:40:16 -0500

    S3L20 - Cleaning UP The Player Code

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,15 +3,27 @@ extends KinematicBody2D
 const SPEED = 750
 const GRAVITY = 3600
 const UP = Vector2(0, -1)
-const JUMP_SPEED = -1750
+const JUMP_SPEED = -1700
 
 var motion = Vector2()
 
 func _physics_process(delta):
+	update_motion(delta)
+
+
+func _process(delta):
+	update_animation(motion)
+
+	
+func update_motion(delta):
 	fall(delta)
 	run()
 	jump()
 	move_and_slide(motion, UP)
+
+
+func update_animation(motion):
+	$AnimatedSprite.update(motion)
 
 
 func fall(delta):
@@ -24,18 +36,13 @@ func fall(delta):
 func run():
 	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
-		$AnimatedSprite.play("run")
-		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		motion.x = -SPEED
-		$AnimatedSprite.play("run")
-		$AnimatedSprite.flip_h = true
 	else:
 		motion.x = 0
-		$AnimatedSprite.play("idle")
-		$AnimatedSprite.flip_h = false
 
 
 func jump():
 	if is_on_floor() and Input.is_action_pressed("ui_up"):
 		motion.y = JUMP_SPEED
+
