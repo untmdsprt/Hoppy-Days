c216d6b39c13ab70c8403022abd83721d8f28924
Jenny <untmdsprt@gmail.com>
2018-07-17 18:42:33 -0500

    S3L32 - Adding Lives

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,17 +8,21 @@ const JUMP_SPEED = -1700
 var motion = Vector2()
 export var world_limit = 3260
 
-func _physics_process(delta):
-	update_motion(delta)
-
-func _process(delta):
-	update_animation(motion)
 
 func update_motion(delta):
 	fall(delta)
 	run()
 	jump()
 	move_and_slide(motion, UP)
+
+
+func _physics_process(delta):
+	update_motion(delta)
+
+func _process(delta):
+	update_animation(motion)
+
+
 
 func update_animation(motion):
 	$AnimatedSprite.update(motion)
@@ -44,4 +48,5 @@ func jump():
 	if is_on_floor() and Input.is_action_pressed("ui_up"):
 		motion.y = JUMP_SPEED
 
-
+func hurt():
+	motion.y = JUMP_SPEED
