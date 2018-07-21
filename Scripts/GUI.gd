1c5de457d9d715cefe11aa087b27392c3f74bdab
Jenny <untmdsprt@gmail.com>
2018-07-20 23:43:12 -0500

    S3L35 - Polishing the GUI

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -3,7 +3,13 @@ extends CanvasLayer
 func _ready():
 	Global.GUI = self
 	
-func update_GUI(lives):
-	$Banner/HBoxContainer/LifeCount.text = str(lives)
+func update_GUI(coins, lives):
+	$Banner/HBoxContainer/CoinCount.text = "%02d" % coins
+	$Banner/HBoxContainer/LifeCount.text = "%02d" % lives
 	
+	# "%02d" adds a zero in front of the number to make it look better
+	
+func animate(animation):
+	$AnimationPlayer.play(animation)
+		
 	
